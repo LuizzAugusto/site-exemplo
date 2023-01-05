43b87e9f7143650a69f31a4b2c631654d0d09921
Luiz Augusto Schneider da Silva <luizz.augustoll@hotmail.com>
2023-01-05 00:13:53 -0400

    Revert "using gist for loadCSS"
    This reverts commit 38f782e2ec306b2f7ae4ad0e403da946965c478e.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 //@ts-check
-import { loadCSS } from "https://gist.githubusercontent.com/LuizzAugusto/ee5ea3a915d236a1dcc1e11ce1d4c06b/raw/0725bbcabe87659d51559ec63e78ab3e01b079e2/loadCSS.min.js";
+import { loadCSS } from "../utils/loadCSS.js";
 import { signalsBag } from "../utils/signalsBag.js";
 import { TopBar } from "./navigation/TopBar.jsx";
 import { SideMenu } from "./navigation/SideMenu.jsx";
--- src/components/navigation/TopBar.jsx
+++ src/components/navigation/TopBar.jsx
@@ -1,5 +1,5 @@
 //@ts-check
-import { loadCSS } from "https://gist.githubusercontent.com/LuizzAugusto/ee5ea3a915d236a1dcc1e11ce1d4c06b/raw/0725bbcabe87659d51559ec63e78ab3e01b079e2/loadCSS.min.js";
+import { loadCSS } from "../../utils/loadCSS.js";
 import { MenuButton } from "../inputs/MenuButton.jsx";
 
 /**
